6fd8a28c720ee7e72cfc916fa622968e726a1e54
oleggr <oleg.gr@outlook.com>
2021-03-14 00:15:34 +0300

    init

---
+++ app/api/router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST
+
+
+router = APIRouter()
+
+
+@router.get("/",)
+async def root():
+    return "Hello World"
+
+
+@router.get("/test",)
+async def test():
+    return "TEST ROUTE!!!!!"
+
+
+@router.get("/{user_id}",)
+async def user(user_id: int):
+    if user_id != 0:
+        return f'Your id is: {user_id}'
+    else:
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+        )
+
---
+++ tests/test_first.py
@@ -0,0 +1,4 @@
+
+def test_first():
+    a = 1
+    assert a == 1
