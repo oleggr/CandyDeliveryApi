9d7adb25ccafd8b1d7b29a766eac70f20db24929
oleggr <oleg.gr@outlook.com>
2021-03-15 03:02:39 +0300

    added 2 test routes

--- app/api/router.py
+++ app/api/router.py
@@ -1,22 +1,45 @@
 from fastapi import APIRouter, HTTPException
 
-from starlette.status import HTTP_400_BAD_REQUEST
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 
+from app.db.services.regions import RegionsService
 
 router = APIRouter()
 
 
-@router.get("/",)
+@router.get("/", )
 async def root():
     return "Hello World"
 
 
-@router.get("/test",)
+@router.get("/test", )
 async def test():
     return "TEST ROUTE!!!!!"
 
 
-@router.get("/{user_id}",)
+@router.post("/region/{region_id}", )
+async def set_region(region_id):
+    try:
+        await RegionsService().add_region(
+            region_id,
+            f'test region {region_id}'
+        )
+        return HTTP_200_OK
+    except:
+        return HTTP_400_BAD_REQUEST
+
+
+@router.get("/region/{region_id}", )
+async def get_region(region_id):
+    try:
+        region = await RegionsService().get_region_by_id(region_id)
+        return f'Your region has id:{region.region_id} name:{region.region_name}'
+
+    except:
+        return HTTP_400_BAD_REQUEST
+
+
+@router.get("/{user_id}", )
 async def user(user_id: int):
     if user_id != 0:
         return f'Your id is: {user_id}'
