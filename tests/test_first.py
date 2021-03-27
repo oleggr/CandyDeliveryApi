284247731e058c7ef4703d0fd17eb8af44192bb8
oleggr <oleg.gr@outlook.com>
2021-03-27 19:58:39 +0300

    basic tests preparation

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,4 +1,34 @@
+import asyncio
+import asynctest
+from fastapi import FastAPI
+from httpx import AsyncClient
+
+from app.db.models.regions import Region
+
 
 def test_first():
     a = 1
     assert a == 1
+
+
+async def test_hello(
+        app: FastAPI,
+        client: AsyncClient
+):
+    response = await client.get(app.url_path_for('hello-world'))
+    print('-->', response.json())
+
+
+# def test_add_region(
+#         app: FastAPI, client: AsyncClient,  test_region: Region
+# ):
+    # await client.post(
+    #     app.url_path_for('region:add-region'),
+
+    # )
+
+    # print('\n>> ', app.url_path_for('hello-world'))
+
+    # print()
+    # for elem in app.routes:
+    #     print('>>', elem.url_path_for(elem.name))
