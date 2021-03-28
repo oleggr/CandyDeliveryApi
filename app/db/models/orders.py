89b85e39be88d33c8aa3666beb82981d616d19b0
oleggr <oleg.gr@outlook.com>
2021-03-28 21:36:16 +0300

    updated orders data models

--- app/db/models/orders.py
+++ app/db/models/orders.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, Field
 
 
 class Order(BaseModel):
     order_id: int
     weight: float
-    region_id: int
-    is_ready: bool
-    complete_time: int
+    region_id: int = Field(None, alias='region')
+    is_ready: bool = False
+    complete_time: int = None
+    assign_id: int = None
 
     @validator('order_id', 'region_id')
     def id_validation(cls, v: int):
@@ -32,3 +33,14 @@ class OrderAssign(BaseModel):
         if not (v >= 0 and isinstance(v, int)):
             raise ValueError('Id must be positive integer')
         return v
+
+
+class OrderFull(Order):
+    delivery_hours: list
+
+    @validator('delivery_hours')
+    def list_validation(cls, v: list):
+        if not len(v) > 0:
+            raise ValueError('Field can\'t be empty')
+        return v
+
